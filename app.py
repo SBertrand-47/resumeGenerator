c2f77bcb3b6f920a437b363cbaae532ff2147115
Bertrand47 <sibomanabertrand@gmail.com>
2023-08-06 04:22:29 -0500

    Made a couple of changes on the format of the resume

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request, make_response
-from flask_weasyprint import HTML, render_pdf
+from flask import Flask, render_template, request, send_file
+import tempfile
+import pdfcrowd
 
 app = Flask(__name__)
+
+app.jinja_env.globals.update(zip=zip)
 
 @app.route('/')
 def home():
@@ -9,33 +12,45 @@ def home():
 
 @app.route('/generate_resume', methods=['POST'])
 def generate_resume():
-    # Get form data
     data = {
         'name': request.form.get('name'),
         'location': request.form.get('location'),
         'email': request.form.get('email'),
         'phone': request.form.get('phone'),
-        'linkedin': request.form.get('linkedin'),
-        'portfolio': request.form.get('portfolio'),
-        'education': request.form.get('education'),
+        'github': request.form.get('github'),
+        'major': request.form.get('major'),
+        'university': request.form.get('university'),
+        'period': request.form.get('period'),
         'skills': request.form.get('skills'),
-        'experience': request.form.get('experience'),
-        'projects': request.form.get('projects'),
-        'awards': request.form.get('awards')
+        'roles': request.form.get('role'),
+        'companies': request.form.get('company'),
+        'experience_periods': request.form.get('experience_period'),
+        'descriptions': request.form.get('description'),
+        'project_names': request.form.get('project_name'),
+        'project_descriptions': request.form.get('project_description'),
+        'awards': request.form.get('awards'),
     }
 
-    # Render the resume.html template with the form data
-    html = render_template('resume.html', **data)
+    # Render the HTML as usual but as a string
+    rendered = render_template('resume.html', **data)
 
-    # Convert the HTML to a PDF
-    pdf = HTML(string=html).write_pdf()
+    # Create a Pdfcrowd API client instance
+    client = pdfcrowd.HtmlToPdfClient('mr47', '23f23109334517f141f7494c8132ecc0')
 
-    # Create a response with the PDF data
-    response = make_response(pdf)
-    response.headers.set('Content-Type', 'application/pdf')
-    response.headers.set('Content-Disposition', 'attachment', filename='resume.pdf')
+    # Set the page margins (top, right, bottom, left) in points
+    client.setPageMargins("2pt", "2pt", "2pt", "2pt")
 
-    return response
 
-if __name__ == "__main__":
+    # Convert HTML string to a PDF
+    pdf = client.convertString(rendered)
+
+    # Create a temporary file to save the PDF
+    pdf_file = tempfile.NamedTemporaryFile(delete=False, suffix=".pdf")
+    pdf_file.write(pdf)
+    pdf_file.close()
+
+    # Send the PDF file as a response
+    return send_file(pdf_file.name, mimetype='application/pdf', as_attachment=True)
+
+if __name__ == '__main__':
     app.run(debug=True)
